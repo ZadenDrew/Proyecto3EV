95d7523a6eb7abbc3e62177222c4fb5f3da5b2f3
acabezaslopez <acabezaslopez@victoria11.danielcastelao.org>
2018-05-28 10:19:13 +0200

    metodo modify funciona

---
+++ src/tienda_videojuegos/Tienda_videojuegos.java
@@ -0,0 +1,18 @@
+
+package tienda_videojuegos;
+
+/**
+ *
+ * @author andrea
+ */
+public class Tienda_videojuegos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Tabla_Juego tb = new Tabla_Juego();
+        tb.setVisible(true);
+    }
+
+}
